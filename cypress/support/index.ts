a6c9ff2b6f5870e18f6972c0c1c29c1027bfd4df
Jessica Barrows-Butler <jessica.b.barrows@gmail.com>
2023-03-31 23:09:01 -0700

    initial project setup

---
+++ cypress/support/index.ts
@@ -0,0 +1,16 @@
+// cypress/support/index.ts
+Cypress.Commands.add("dataCy", (value) => {
+  return cy.get(`[data-cy=${value}]`);
+});
+
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      /**
+       * Custom command to select DOM element by data-cy attribute.
+       * @example cy.dataCy('greeting')
+       */
+      dataCy(value: string): Chainable<JQuery<HTMLElement>>;
+    }
+  }
+}
